0d7fef9e4747e77f3e1615ad62a2005cbd31b8bd
YVictorin <vicyonjou@gmail.com>
2025-02-12 18:05:27 -0500

    refactor: update styling and navbar path to quiz page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,12 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-<<<<<<< HEAD
-    <footer className="page-footer light-blue darken-2" style={{marginTop: "8rem"}}>
-      <div style={{marginLeft: "2rem"}}>
-=======
     <footer
       className="page-footer light-blue darken-2"
       style={{ marginTop: "8rem" }}
     >
       <div className="container">
->>>>>>> 6500a8772111754c9d77f5fac7366bdfac2d0ce5
         <div className="row">
           <div className="col l6 s12">
             <p className="grey-text text-lighten-4">
@@ -24,20 +19,15 @@ export default function Footer() {
           <div className="col l4 offset-l2 s12">
             <h5 className="white-text">Links</h5>
             <ul>
-              {/* <li><Link className="grey-text text-lighten-3" to="/link1">Home</Link> </li> */}
-              {/* <li><Link className="grey-text text-lighten-3" to="/link2">Quiz Generator</Link></li> */}
-              {/* <li><Link className="grey-text text-lighten-3" to="/link3">Account</Link></li> */}
+              <li><Link className="grey-text text-lighten-3" to="/">Home</Link> </li>
+              <li><Link className="grey-text text-lighten-3" to="/quiz-gen">Quiz Generator</Link></li>
+              <li><Link className="grey-text text-lighten-3" to="/account">Account</Link></li>
             </ul>
           </div>
         </div>
       </div>
       <div className="footer-copyright">
-<<<<<<< HEAD
-        <div style={{marginLeft: "2rem"}}> © 2024 Copyright Text
-        </div>
-=======
         <div className="container"> © 2024 Copyright Text</div>
->>>>>>> 6500a8772111754c9d77f5fac7366bdfac2d0ce5
       </div>
     </footer>
   );
