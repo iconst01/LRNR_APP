3f31baed7242e29425bd6af8c89c4f401fd0a5d4
AngelDuerto <ajdupe08@gmail.com>
2025-02-17 23:35:51 -0500

    Refactor: Delete comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import QuizGen from "./pages/QuizGen";
-// import QuizPage from "./pages/QuizPage";
+import QuizPage from "./pages/QuizPage";
 import Account from "./pages/Account";
 // import Results from "./pages/Results";
 
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/account" element={<Account />} />
         <Route path="/quiz-gen" element={<QuizGen />} />
-        {/* <Route path="/quiz-page" element={<QuizPage />} /> */}
+        <Route path="/quiz-page" element={<QuizPage />} />
         {/* <Route path="/results" element={<Results />} /> */}
       </Routes>
       <Footer />
