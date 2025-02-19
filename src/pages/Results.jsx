11651c166da9b1795a40d039b1773c2fe1d81ad1
AngelDuerto <ajdupe08@gmail.com>
2025-02-18 21:56:57 -0500

    Feat: Add Results.jsx page

---
+++ src/pages/Results.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import "../styles/Results.css";
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
+
+const ResultsPage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="section container center-align">
+        <h2 className="center-align teal-text text-darken-5">lrnr</h2>
+        <h5>Questions Right: 0111</h5>
+        <div className="section">
+        <button className="btnTry waves-effect waves-light teal darken-1 btn-large" onClick={() => navigate("/quiz-gen")}>
+            TRY ANOTHER QUIZ
+        </button>
+        </div>
+        </div>
+    );
+};
+
+export default ResultsPage;
